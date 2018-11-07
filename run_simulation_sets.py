fb41b1c21205d2d6f91e666bde11baa24853a7df
DianaS <1442210@my.brunel.ac.uk>
2018-11-07 12:28:36 +0000

    Modified run_simulation_sets.py ensemble simulations

--- run_simulation_sets.py
+++ run_simulation_sets.py
@@ -4,16 +4,16 @@ from plugins.FabFlee.FabFlee import *
 @task
 def run_ssudan_paper():
   for postfix in ["default","reg","links","ccamp","cborder","redirect","adjumani1","adjumani2"]:  
-    #flee("ssudan_%s" % (postfix),simulation_period=604)
-    #plot_output("ssudan_%s_localhost_16" % (postfix),"out")
+    flee("ssudan_%s" % (postfix),simulation_period=604)
+    plot_output("ssudan_%s_localhost_16" % (postfix),"out")
 
-    test_sensitivity("ssudan_%s" % (postfix),simulation_period=604,name="MaxMoveSpeed",values="150-200-250")
-    for i in [150, 200, 250]:
+    test_sensitivity("ssudan_%s" % (postfix),simulation_period=604,name="MaxMoveSpeed",values="25-50-100-150-200-250")
+    for i in [25, 50, 100, 150, 200, 250]:
       plot_output("ssudan_%s_MaxMoveSpeed_%s_localhost_16" % (postfix, i),"out")
 
-    #test_sensitivity("ssudan_%s" % (postfix),simulation_period=604,name="Awareness",values="0-1-2")
-    #for i in [0, 1, 2]:
-      #plot_output("ssudan_%s_Awareness_%s_localhost_16" % (postfix, i),"out")
+    test_sensitivity("ssudan_%s" % (postfix),simulation_period=604,name="Awareness",values="0-1-2")
+    for i in [0, 1, 2]:
+      plot_output("ssudan_%s_Awareness_%s_localhost_16" % (postfix, i),"out")
 
 
 
