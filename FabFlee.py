af89f2f3d3e81557728355e7bd0acaca7f2561c6
Hamid Arabnejad <hamid.arabnejad@gmail.com>
2019-12-16 10:51:35 +0000

    Update FabFlee.py

--- FabFlee.py
+++ FabFlee.py
@@ -912,6 +912,6 @@ def test_sensitivity(config, **args):
 
 # Test Functions
 # from plugins.FabFlee.test_FabFlee import *
-from plugins.FabFlee.run_simulation_sets import *
-from plugins.FabFlee.run_flee_easyvvuq import *
-from plugins.FabFlee.run_perf_benchmarks import *
+# from plugins.FabFlee.run_simulation_sets import *
+# from plugins.FabFlee.run_flee_easyvvuq import *
+# from plugins.FabFlee.run_perf_benchmarks import *
