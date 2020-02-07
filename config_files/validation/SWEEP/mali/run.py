b4be60838f5536b87e07cdc1bef2db0fa62effc2
Derek Groen <djgroennl@gmail.com>
2020-02-07 15:08:56 +0000

    Updated files for Mali.

--- config_files/validation/SWEEP/mali/run.py
+++ config_files/validation/SWEEP/mali/run.py
@@ -1,5 +1,5 @@
 from flee import flee
-from datamanager import handle_refugee_data
+from datamanager import handle_refugee_data,read_period
 from datamanager import DataTable #DataTable.subtract_dates()
 from flee import InputGeography
 import numpy as np
@@ -12,17 +12,11 @@ def AddInitialRefugees(e, d, loc):
   for i in range(0, num_refugees):
     e.addAgent(location=loc)
 
-start_date = "2012-02-29"
 insert_day0_refugees_in_camps = True
-
-def date_to_sim_days(date):
-  return DataTable.subtract_dates(date,start_date)
-
 
 if __name__ == "__main__":
 
-  end_time = 300
-  last_physical_day = 300
+  start_date,end_time = read_period.read_conflict_period("{}/conflict_period.csv".format(sys.argv[1]))
 
   if len(sys.argv)<4:
     print("Please run using: python3 run.py <your_csv_directory> <your_refugee_data_directory> <duration in days> <optional: simulation_settings.csv> > <output_directory>/<output_csv_filename>")
@@ -31,7 +25,6 @@ if __name__ == "__main__":
   validation_data_directory = sys.argv[2]
   if int(sys.argv[3]) > 0:
     end_time = int(sys.argv[3])
-    last_physical_day = int(sys.argv[3])
 
   if len(sys.argv)==5:
     flee.SimulationSettings.ReadFromCSV(sys.argv[4])
@@ -50,8 +43,6 @@ if __name__ == "__main__":
   ig.ReadClosuresFromCSV("%s/closures.csv" % input_csv_directory)
 
   e,lm = ig.StoreInputGeographyInEcosystem(e)
-
-  #print("Network data loaded")
 
   d = handle_refugee_data.RefugeeTable(csvformat="generic", data_directory=validation_data_directory, start_date=start_date, data_layout="data_layout.csv")
 
@@ -137,7 +128,5 @@ if __name__ == "__main__":
       output += ",%s,%s,%s,%s,%s,%s" % (float(np.sum(abs_errors))/float(refugees_raw), int(sum(loc_data)), e.numAgents(), refugees_raw, refugees_in_camps_sim, refugee_debt)
     else:
       output += ",0,0,0,0,0,0"
-      #output_string += ",0"
-
 
     print(output)
