a06640251c71c711e26c364792db01d409ee915e
Hamid <hamid.arabnejad@gmail.com>
2020-09-06 20:38:22 +0100

    Add file suffix option

--- scripts/run_flare.py
+++ scripts/run_flare.py
@@ -7,27 +7,30 @@ if __name__ == "__main__":
 
     end_time = 100
 
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         if (sys.argv[1]).isnumeric():
             end_time = int(sys.argv[1])
 
-    if len(sys.argv)>2:
+    if len(sys.argv) > 2:
         input_dir = sys.argv[2]
     else:
         input_dir = "test_input_csv"
 
-
-    if len(sys.argv)>3:
+    if len(sys.argv) > 3:
         out_file = sys.argv[3]
     else:
         out_file = "flare-out.csv"
 
+    if len(sys.argv) > 4:
+        file_suffix = sys.argv[4]
+    else:
+        file_suffix = ""
 
     ig = InputGeography.InputGeography()
 
-    ig.ReadLocationsFromCSV("%s/locations.csv" % input_dir)
+    ig.ReadLocationsFromCSV("%s/locations%s.csv" % (input_dir, file_suffix))
 
-    ig.ReadLinksFromCSV("%s/routes.csv" % input_dir)
+    ig.ReadLinksFromCSV("%s/routes%s.csv" % (input_dir, file_suffix))
 
     e = Ecosystem.Ecosystem()
 
@@ -35,17 +38,18 @@ if __name__ == "__main__":
 
     #print("Network data loaded")
 
-    file = open("%s" % out_file,"w")
+    print("output file -> %s" % (out_file))
+    file = open("%s" % out_file, "w")
 
     output_header_string = "#Day,"
 
     for l in e.locations:
         output_header_string += " %s," % (l.name)
-    
+
     output_header_string += "\n"
     file.write(output_header_string)
 
-    for t in range(0,end_time):
+    for t in range(0, end_time):
 
         e.evolve()
 
@@ -53,9 +57,9 @@ if __name__ == "__main__":
 
         for l in e.locations:
             if l.flare:
-                output +=",1"
-            else: 
-                output +=",0"
+                output += ",1"
+            else:
+                output += ",0"
 
         output += "\n"
         file.write(output)
