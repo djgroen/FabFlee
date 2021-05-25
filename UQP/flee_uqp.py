f6f225bcf882c5f022a2649919c61923fc41d376
arabnejad <hamid.arabnejad@gmail.com>
2021-05-25 11:33:51 +0100

    Adopts with new FabSim3 refactoring

--- UQP/flee_uqp.py
+++ UQP/flee_uqp.py
@@ -1,4 +1,8 @@
-from base.fab import *
+try:
+    from fabsim.base.fab import *
+except ImportError:
+    from base.fab import *
+
 import numpy as np
 import os
 import sys
@@ -10,16 +14,17 @@ import UQP.uqp as uqp
 
 # Source file for UQP examples FabFlee.
 
+
 def _flee_uqp(config_info, replicas, args):
-  flee(config_info["config"], config_info["simulation_period"], replicas=replicas, args)
-  fetch_results()
-  return template(env.job_name_template)
+    flee(config_info["config"], config_info["simulation_period"],
+         replicas=replicas, args)
+    fetch_results()
+    return template(env.job_name_template)
+
 
 def _flee_collate(output_directory):
-  validate_flee_output(output_directory)
+    validate_flee_output(output_directory)
 
 
-def test_uqp1(config,simulation_period,replicas=10, **args):
-   uqp1_aleatoric(config, _flee_uqp, _flee_collate, replicas, args)
-
-
+def test_uqp1(config, simulation_period, replicas=10, **args):
+    uqp1_aleatoric(config, _flee_uqp, _flee_collate, replicas, args)
--- run_perf_benchmarks.py
+++ run_perf_benchmarks.py
@@ -1,8 +1,14 @@
-from base.fab import *
+try:
+    from fabsim.base.fab import *
+except ImportError:
+    from base.fab import *
+
 from plugins.FabFlee.FabFlee import *
+
 
 @task
 def flee_get_perf(results_dir):
-    print("{}/{}".format(env.local_results,results_dir))
-    my_file = open("{}/{}/perf.log".format(env.local_results,results_dir), 'r')
+    print("{}/{}".format(env.local_results, results_dir))
+    my_file = open(
+        "{}/{}/perf.log".format(env.local_results, results_dir), 'r')
     print(my_file.read())
