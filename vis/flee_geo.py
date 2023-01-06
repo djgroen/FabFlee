c8b651b303c4fc0a7c2c14db19eb3c8ca40100ae
Derek Groen <djgroennl@gmail.com>
2023-01-06 15:28:59 +0000

    Added automated agent plotting.

--- vis/flee_geo.py
+++ vis/flee_geo.py
@@ -13,6 +13,9 @@ import sys
 
 @task
 def plot_flee_links(config):
+    """
+    Plots the location graph in a configuration directory.
+    """
     with_config(config)
 
     #print(env)
@@ -44,6 +47,83 @@ def plot_flee_links(config):
     )
     fig.update_layout(height=800, margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
+
+
+@task
+def plot_flee_agents(results_dir, proc="0", agentid="0"):
+    """
+    Plots the trajectory of an individual agent in Flee
+    """
+
+    agentcsv = "{}/{}/agents.out.{}".format(env.local_results, results_dir, proc)
+    lats = []
+    lons = []
+    names = []
+    colors = []
+
+    count = 0
+
+    with open(agentcsv, newline='') as csvfile:
+        header = []
+        agentreader = csv.reader(csvfile, delimiter=',')
+        for row in agentreader:
+            if len(row) < 2:
+                pass
+            elif count == 0:
+                header = row
+                header_not_read = False
+                print(header)
+                count += 1
+            else:
+                if(row[1]) == "{}-{}".format(proc, agentid):
+                    if count % 10 == 0:
+                        lats = np.append(lats, [float(row[4]), None, float(row[4])])
+                        lons = np.append(lons, [float(row[5]), None, float(row[5])])
+                        names = np.append(names, [row[0], None, row[0]])
+                    else:
+                        lats = np.append(lats, [float(row[4])])
+                        lons = np.append(lons, [float(row[5])])
+                        names = np.append(names, [row[0]])
+                    count += 1
+
+    print(colors)
+    fig = px.scatter_geo(lat=lats,lon=lons, hover_name=names)
+
+    interval=30
+    for i in range(0,int((len(lats)+interval-1)/interval)):
+        start = i*interval
+        end = (i+1)*interval
+        fig.add_scattergeo(
+            name = '{}-{}'.format(start, end),
+            lon = lons[start:end],
+            lat = lats[start:end],
+            hoverinfo = 'text',
+            text = names[start:end],
+            mode = 'lines',
+            line = dict(width = 4)
+        )
+
+    fig.update_geos(fitbounds="locations")
+    fig.update_geos(
+        visible=False, resolution=50,
+        showcountries=True, countrycolor="RebeccaPurple"
+    )
+    fig.update_layout(height=800) #, margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(
+        title="Agent movement graph",
+        xaxis_title="Longitude",
+        yaxis_title="Latitude",
+        legend_title="Agent",
+        font=dict(
+            family="Courier New, monospace",
+            size=32,
+            color="Black"
+            )
+        )
+
+    fig.show()
+
+
 
 
 if __name__ == "__main__":
