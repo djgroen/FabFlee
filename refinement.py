ef45079a921120fb97fa6c1cc57bffde161cc853
cspgdds <diana.suleimenova@brunel.ac.uk>
2025-03-06 17:04:20 +0000

    Fixed refinement.py to run refinement commands within config_files.

--- refinement.py
+++ refinement.py
@@ -3,17 +3,6 @@ try:
 except ImportError:
     from base.fab import *
 
-# Import V&V primitives.
-try:
-    import fabsim.VVP.vvp as vvp
-except ImportError:
-    import VVP.vvp as vvp
-
-try:
-    from FabFlee import *
-except ImportError:
-    from FabFlee import *
-
 import glob
 import csv
 import os
@@ -24,74 +13,21 @@ from shutil import copyfile, rmtree, move
 add_local_paths("FabFlee")
 
 @task
-# Syntax: fabsim localhost clear_active_conflict
-def clear_active_conflict():
-    """ 
-    Delete all content in the active conflict directory. 
-    """
-
-    local(template("rm -rf %s/conflict_data/active_conflict/"
-                   % (get_plugin_path("FabFlee"))))
-
-@task
-# Syntax: fabsim localhost load_conflict:<conflict_name>
-def load_conflict(conflict_name):
-    """
-    Load source data and flee csv files for a specific conflict from
-    conflict data to active conflict directory.
-    """
-    # copies *.csv files from $FabFlee/conflict_data/<conflict_name> to
-    # $FabFlee/conflict_data/active_conflict.
-
-    # 1. Load locations.csv, routes.csv and closures.csv files which
-    # correspond to a specific conflict.
-    # These CSV will be store in $FabFlee/conflict_data. Each conflict will be
-    # stored in a separate folder.
-
-    # 2. Move these CSVs to an "active_conflict" directory.
-    # This is located in $FABSIM/conflict_data/active_conflict.
-    local(template("mkdir -p %s/conflict_data/active_conflict"
-                   % (get_plugin_path("FabFlee"))))
-
-    local(template("cp %s/conflict_data/%s/*.csv \
-        %s/conflict_data/active_conflict/")
-          % (get_plugin_path("FabFlee"), conflict_name,
-             get_plugin_path("FabFlee")))
-
-    local(template("mkdir -p %s/conflict_data/active_conflict/source_data"
-                   % (get_plugin_path("FabFlee"))))
-
-    local(template("cp %s/conflict_data/%s/source_data/*.csv \
-        %s/conflict_data/active_conflict/source_data/")
-          % (get_plugin_path("FabFlee"), conflict_name,
-             get_plugin_path("FabFlee")))
-
-    local(template("cp %s/config_files/run.py \
-        %s/conflict_data/active_conflict")
-          % (get_plugin_path("FabFlee"), get_plugin_path("FabFlee")))
-
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost load_conflict:%s\n" % conflict_name)
-
-
-@task
-# Syntax: fabsim localhost add_camp:camp_name,region,country(,lat,lon)
-def add_camp(camp_name, region=" ", country=" ", lat=0.0, lon=0.0):
+# Syntax: fabsim localhost add_camp:<config_name>,<camp_name>,<region>,<country>(,<lat>,<lon>)
+def add_camp(config, camp_name, region=" ", country=" ", lat=0.0, lon=0.0):
     """ 
     Add an additional new camp to locations.csv. 
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost add_camp:%s\n" % camp_name)
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost add_camp:%s\n" % camp_name)
 
     # 1. Add (or make existing forwarding hub) a new camp to locations.csv
     # If new camp, add country,lat,lon,location_type(camp)
     # If existing camp, change location_type to camp
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/locations.csv"
-                        % (get_plugin_path("FabFlee")), "r"))
+    r = csv.reader(open("%s/config_files/%s/input_csv/locations.csv" % (get_plugin_path("FabFlee"), config), "r"))
     lines = [l for l in r]
 
     for i in range(1, len(lines)):
@@ -103,32 +39,32 @@ def add_camp(camp_name, region=" ", country=" ", lat=0.0, lon=0.0):
 
     # 2. Append one line to lines, containing the details of the new camp.
     add_camp = [camp_name, region, country, lat, lon, "camp"]
-    with open("%s/conflict_data/active_conflict/locations.csv"
-              % (get_plugin_path("FabFlee")), "a") as new_csv:
+    with open("%s/config_files/%s/input_csv/locations.csv"
+              % (get_plugin_path("FabFlee"), config), "a") as new_csv:
         writer = csv.writer(new_csv)
         writer.writerow(add_camp)
     print(add_camp)
 
 @task
 # Syntax: fabsim localhost delete_location:<location_name>
-def delete_location(location_name):
+def delete_location(config, location_name):
     """ 
     Deletes not-required camp (or location) from locations.csv. 
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost delete_location:%s\n" % location_name)
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost delete_location:%s\n" % location_name)
 
     # 1. Delete camp from locations.csv containing the details of the camp.
     # 2. Write the updated CSV file.
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/locations.csv"
-                        % (get_plugin_path("FabFlee")), "r"))
+    r = csv.reader(open("%s/config_files/%s/input_csv/locations.csv"
+                        % (get_plugin_path("FabFlee"), config), "r"))
     lines = [l for l in r]
 
-    writer = csv.writer(open("%s/conflict_data/active_conflict/locations.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    writer = csv.writer(open("%s/config_files/%s/input_csv/locations.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
 
     for i in range(0, len(lines)):
         if lines[i][0].strip() != location_name:
@@ -147,21 +83,21 @@ def delete_location(location_name):
 
 @task
 # Syntax: fabsim localhost change_distance:name1,name2,distance
-def change_distance(source, destination, distance):
+def change_distance(config, source, destination, distance):
     """ 
     Change distance between two locations in routes.csv. 
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost change_distance:%s,%s,%s\n"
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost change_distance:%s,%s,%s\n"
                      % (source, destination, distance))
 
     # 1. Read routes.csv and for each location in the dict, find in the csv,
     # and change distance between two locations.
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/routes.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/routes.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
 
     for i in range(1, len(lines)):
@@ -172,28 +108,28 @@ def change_distance(source, destination, distance):
         lines[i][2] = distance
         print(lines[i])
 
-    # 2. Write the updated closures.csv in the active_conflict directory.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/routes.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    # 2. Write the updated closures.csv.
+    writer = csv.writer(open("%s/config_files/%s/input_csv/routes.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
 @task
 # Syntax: fabsim localhost close_camp:camp_name,country(,closure_start,closure_end)
-def close_camp(camp_name, country, closure_start=0, closure_end=-1):
+def close_camp(config, camp_name, country, closure_start=0, closure_end=-1):
     """ 
     Close camp located within neighbouring country. 
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost close_camp:%s,%s\n" % (camp_name, country))
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost close_camp:%s,%s\n" % (camp_name, country))
 
     # 1. Change closure_start and closure_end or add a new
     # camp closure to closures.csv.
     # Format: closure type <location>,name1,name2,closure_start,closure_end
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/closures.csv"
-                        % (get_plugin_path("FabFlee"))))  # Here your csv file
+    r = csv.reader(open("%s/config_files/%s/input_csv/closures.csv"
+                        % (get_plugin_path("FabFlee"), config)))  # Here your csv file
     lines = [l for l in r]
     camp_found = False
 
@@ -214,30 +150,30 @@ def close_camp(camp_name, country, closure_start=0, closure_end=-1):
                       closure_start, closure_end])
     # print(lines)
 
-    # 2. Write the updated closures.csv in the active_conflict directory.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/closures.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    # 2. Write the updated closures.csv in the %s directory.
+    writer = csv.writer(open("%s/config_files/%s/input_csv/closures.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
 @task
 # Syntax: fabsim localhost close_border:country1,country2(,closure_start,closure_end)
-def close_border(country1, country2, closure_start=0, closure_end=-1):
+def close_border(config, country1, country2, closure_start=0, closure_end=-1):
     """
     Close border between conflict country and camps located
     within specific neighbouring country.
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost close_border:%s,%s\n"
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost close_border:%s,%s\n"
                      % (country1, country2))
 
     # 1. Change closure_start and closure_end or add a new camp
     # closure to closures.csv.
     # Format: closure type <country>,name1,name2,closure_start,closure_end
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/closures.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/closures.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
     border_found = False
 
@@ -258,21 +194,21 @@ def close_border(country1, country2, closure_start=0, closure_end=-1):
                       closure_start, closure_end])
 
     """
-    local(template("cp %s/conflict_data/%s/*.csv \
-        %s/conflict_data/active_conflict/")
+    local(template("cp %s/config_files/%s/input_csv/*.csv \
+        %s/config_files/%s/")
           % (get_plugin_path("FabFlee"), conflict_name,
              get_plugin_path("FabFlee")))
     print(lines)
     """
 
-    # 2. Write the updated closures.csv in the active_conflict directory.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/closures.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    # 2. Write the updated closures.csv.
+    writer = csv.writer(open("%s/config_files/%s/input_csv/closures.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
 @task
 # Syntax: fabsim localhost change_capacities:camp_name=capacity(,camp_name2=capacity2)
-def change_capacities(**capacities):
+def change_capacities(config, **capacities):
     """
     Change the capacity of a set of camps in the active conflict directory.
     """
@@ -281,17 +217,17 @@ def change_capacities(**capacities):
     capacities_string = ""
     for c in capacities.keys():
         capacities_string += "%s=%s" % (c, capacities[c])
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost change_capacities:%s\n"
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost change_capacities:%s\n"
                      % capacities_string)
 
     # 1. Read in locations.csv
     # 2. for each location in the dict, find it in the csv, and modify the
     # population value accordingly.
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/locations.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/locations.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
 
     for camp_name in capacities.keys():
@@ -306,27 +242,27 @@ def change_capacities(**capacities):
             print(lines[i])
 
     # 3. Write the updated CSV file.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/locations.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    writer = csv.writer(open("%s/config_files/%s/input_csv/locations.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
 @task
 # Syntax: fabsim localhost redirect:location_name1,location_name2
-def redirect(source, destination):
+def redirect(config, source, destination):
     """
     Redirect from town or (small/other)camp to (main)camp.
     """
 
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
         myfile.write("fabsim localhost redirect:%s,%s\n" % (source, destination))
 
     # 1. Read locations.csv and for each location in the dict, find in the csv,
     # and redirect refugees from location in neighbouring country to camp.
     # 2. Change location_type of source location to forwarding_hub.
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/locations.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/locations.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
 
     for i in range(1, len(lines)):
@@ -337,14 +273,14 @@ def redirect(source, destination):
         print(lines[i])
 
     # 3. Write the updated CSV file.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/locations.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    writer = csv.writer(open("%s/config_files/%s/input_csv/locations.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
     # 4. Find the route from source to destination in routes.csv, and enable
     # forced_redirection.
-    r = csv.reader(open("%s/conflict_data/active_conflict/routes.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/routes.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
 
     for i in range(1, len(lines)):
@@ -364,26 +300,26 @@ def redirect(source, destination):
         print(lines[i])
 
     # 5. Write the updated CSV file.
-    writer = csv.writer(open("%s/conflict_data/active_conflict/routes.csv"
-                             % (get_plugin_path("FabFlee")), "w"))
+    writer = csv.writer(open("%s/config_files/%s/input_csv/routes.csv"
+                             % (get_plugin_path("FabFlee"), config), "w"))
     writer.writerows(lines)
 
 @task
 # Syntax: fabsim localhost add_new_link:<name1>,<name2>,<distance>
-def add_new_link(name1, name2, distance):
+def add_new_link(config, name1, name2, distance):
     """  
     Add a new link between locations to routes.csv. 
     """
-    with open("%s/conflict_data/active_conflict/commands.log.txt"
-              % (get_plugin_path("FabFlee")), "a") as myfile:
-        myfile.write("fab localhost add_new_link:%s,%s,%s\n"
+    with open("%s/config_files/%s/commands.log.txt"
+              % (get_plugin_path("FabFlee"), config), "a") as myfile:
+        myfile.write("fabsim localhost add_new_link:%s,%s,%s\n"
                      % (name1, name2, distance))
 
     # 1. Read routes.csv and for each location in the dict, find in the csv,
     # and change distance between two locations.
     import csv
-    r = csv.reader(open("%s/conflict_data/active_conflict/routes.csv"
-                        % (get_plugin_path("FabFlee"))))
+    r = csv.reader(open("%s/config_files/%s/input_csv/routes.csv"
+                        % (get_plugin_path("FabFlee"), config)))
     lines = [l for l in r]
 
     for i in range(1, len(lines)):
@@ -396,22 +332,22 @@ def add_new_link(name1, name2, distance):
 
     # 2. Append one line to lines, containing the details of links.
     add_new_link = [name1, name2, distance]
-    with open("%s/conflict_data/active_conflict/routes.csv"
-              % (get_plugin_path("FabFlee")), "a") as new_csv:
+    with open("%s/config_files/%s/input_csv/routes.csv"
+              % (get_plugin_path("FabFlee"), config), "a") as new_csv:
         writer = csv.writer(new_csv)
         writer.writerow(add_new_link)
     print(add_new_link)
 
 @task
 # Syntax: fabsim localhost find_capacity:<csv_name>
-def find_capacity(csv_name):
+def find_capacity(config, csv_name):
     """
     Find the highest refugee number within csv file of source data
     for neighbouring camps.
     """
 
     import csv
-    csv_file = open("%s/conflict_data/active_conflict/source_data/%s"
-                    % (get_plugin_path("FabFlee"), csv_name)).readlines()
+    csv_file = open("%s/config_files/%s/source_data/%s"
+                    % (get_plugin_path("FabFlee"), config, csv_name)).readlines()
     print(max(((i, int(l.split(',')[1])) for i, l in enumerate(
         csv_file)), key=lambda t: t[1])[1])
